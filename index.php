023ac9eefe933130ef0994b39315fa51e6f6c889
StephanGi <Stephan.Gillmann@t-online.de>
2017-11-13 18:44:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP-Test</title>
  </head>
  <body>
- <?php echo '<p>Hallo Welt3!!!!!!!!!!!!!!</p>'; ?>
+ <?php echo '<p>Hallo Welt3!!!!!!!!!!!!!!</p> The PHP is running on Pod '".gethostname()."''; ?>
  </body>
 </html>
